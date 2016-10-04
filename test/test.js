f14f7934c2855e24c4d68aba752ae769a8ea6ab2
Ernani <ernani@localhost.localdomain>
2016-10-04 19:42:25 -0300

    added simple test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var irecarga = require('../')({
+    username: process.env.IRECARGA_USERNAME,
+    password: process.env.IRECARGA_PASSWORD
+})
+
+irecarga.validateLoginSenha(function(err, data){
+    console.log('err: ', err)
+    console.log('data', data)
+})
