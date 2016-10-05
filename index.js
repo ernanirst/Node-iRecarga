472a099a18f9b7606edf082fddea457c91307da6
Ernani <ernani@localhost.localdomain>
2016-10-05 18:28:49 -0300

    code refactor and token checking for existing functions

--- index.js
+++ index.js
@@ -1,33 +1,74 @@
-const base_uri = 'https://www.irecarga.com.br/api/ValidateUser.ashx?action='
-const validateLoginPassword = base_uri + 'validateLoginSenha'
+const base_uri = 'https://www.irecarga.com.br/api/'
+const validateLoginPassword = base_uri + 'ValidateUser.ashx?action=validateLoginSenha'
+const reloaderCompanyService = base_uri + 'ReloadCompany.ashx?action=reloaderCompanyService'
 
-var post_handler = require('./post_handler')
+const post_handler = require('./post_handler')
 
 var _this = module.exports = function(credentials) {
     var methods = {
         token: null,
+        /**
+         * update token and returns a json object
+         * */
         validateLoginSenha: function(callback) {
             post_handler.getJSONHttpPost(validateLoginPassword, 
                 {NN_LOGIN: credentials.username, NN_PASSWORD: credentials.password},
                 function(err, data){
-                    methods.token = data.token
-                    callback(err, data)
+                    // check connection
+                    if(err.status === true){
+                        throw 'Could not reach irecarga.com.br'
+                    }else{
+                        // check credentials
+                        if(data.status === false){
+                            throw 'Invalid credentials'
+                        }else{
+                            // store token 
+                            methods.token = data.token
+                            callback(err, data)
+                        }
+                    }
                 }
             )
-        }
+        },
+        /**
+         * returns a list of providers available for a
+         * specific DDD
+         * */
+        getServiceProviders: function(ddd, callback) {
+            // check if has token before proceed
+            if(methods.token == undefined || methods.token == null){
+                // get token
+                methods.validateLoginSenha(function(err, data){
+                    post_handler.getJSONHttpPost(reloaderCompanyService, 
+                        {NN_TOKEN: methods.token, NN_DDD: ddd},
+                        callback
+                    )
+                })
+            }else{
+                // has token
+                post_handler.getJSONHttpPost(reloaderCompanyService, 
+                    {NN_TOKEN: methods.token, NN_DDD: ddd},
+                    function(err, data){
+                        // it doesn't return status if there is no error
+                        if(data.hasOwnProperty('status')){
+                            // get token and post again
+                            methods.validateLoginSenha(function(err, data){
+                                post_handler.getJSONHttpPost(reloaderCompanyService, 
+                                    {NN_TOKEN: methods.token, NN_DDD: ddd},
+                                    callback
+                                )
+                            })
+                        }else{
+                            callback(err, data)
+                        }
+                    }
+                )
+            }
+            
+        },
     }
 
     // checking if credentials are valid
-    methods.validateLoginSenha(function(err, data){
-        if(err.status === true){
-            throw 'Could not reach irecarga.com.br'
-        }else{
-            if(data.status === false){
-                throw 'Invalid credentials'
-            }/*else{
-                methods.token = data.token
-            }*/
-        }
-    })
+    methods.validateLoginSenha(function(err, data){})
     return methods
 }
--- test/test.js
+++ test/test.js
@@ -1,11 +1,18 @@
+// module declaration
 var irecarga = require('../')({
     username: process.env.IRECARGA_USERNAME,
     password: process.env.IRECARGA_PASSWORD
 })
 
+// update token and returns some profile data
 irecarga.validateLoginSenha(function(err, data){
     console.log('err: ', err)
     console.log('data', data)
     console.log('token', irecarga.token)
 })
 
+// get providers for a specific DDD
+irecarga.getServiceProviders(48, function(err, data){
+    console.log('err: ', err)
+    console.log('data', data)
+})
